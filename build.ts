8b597a2622604d533e91210effd615233739b641
Menci <huanghaorui301@gmail.com>
2021-12-19 15:51:13 +0800

    Add hash in URL query

--- build.ts
+++ build.ts
@@ -1,7 +1,9 @@
 import cheerio from "cheerio";
 import klaw from "klaw";
-import fs from "fs/promises";
-import path from "path";
+import fs from "fs";
+import path from "path/posix";
+import url from "url";
+import { crc32 } from "node-crc";
 
 import { isRelativeUrl } from "./utils";
 
@@ -13,31 +15,57 @@ klaw(ROOT_PATH).on("data", item => {
     process(item.path);
 });
 
+const hashCache = new Map<string, string>();
+const hash = async (filePath: string) => {
+  if (hashCache.has(filePath)) return hashCache.get(filePath);
+  const hashValue = crc32(await fs.promises.readFile(filePath)).toString("base64url");
+  hashCache.set(filePath, hashValue);
+  return hashValue;
+};
+
 const process = async (htmlFilePath: string) => {
   const htmlFileRelativePath = path.relative(ROOT_PATH, htmlFilePath);
-  const currentHtmlCdnUrl = new URL(htmlFileRelativePath, CDN_ROOT);
+  // The file:// URL with ROOT_PATH as root
+  const htmlFileFileUrl = url.pathToFileURL("/" + htmlFileRelativePath);
+  const htmlFileCdnUrl = new URL(htmlFileRelativePath, CDN_ROOT);
   
   const $ = cheerio.load(
-    await fs.readFile(htmlFilePath, "utf-8")
+    await fs.promises.readFile(htmlFilePath, "utf-8")
   );
 
-  const processTag = (selector: string, attributeName: string, filter?: (tag: cheerio.Cheerio) => boolean) => {
-    $(selector).each((_, element) => {
-      if (filter && !filter($(element))) return;
+  const processTag = (selector: string, attributeName: string, filter?: (tag: cheerio.Cheerio) => boolean) =>
+    Promise.all(
+      $(selector).toArray().map(async element => {
+        if (filter && !filter($(element))) return;
 
-      const url = $(element).attr(attributeName);
-      if (!url) return;
+        const originalUrl = $(element).attr(attributeName);
+        if (!originalUrl) return;
 
-      if (isRelativeUrl(url)) {
-        $(element).attr(attributeName, new URL(url, currentHtmlCdnUrl).toString());
-      }
-    });
-  };
+        if (isRelativeUrl(originalUrl)) {
+          // Resolve referenced asset file's path
+          const resolvedFileUrl = new URL(originalUrl, htmlFileFileUrl)
+          const resolvedVirtualPath = url.fileURLToPath(resolvedFileUrl);
+          const resolvedPath = path.join(ROOT_PATH, resolvedVirtualPath);
+          if (!fs.existsSync(resolvedPath)) {
+            console.warn(`404 Not Found: ${resolvedVirtualPath}, referenced in ${htmlFileRelativePath}, attr value ${originalUrl}`);
+            return;
+          }
+          const fileHash = await hash(resolvedPath);
 
-  processTag("script", "src");
-  processTag("link[rel=stylesheet]", "href");
-  processTag("img", "src");
+          const resolvedCdnUrl = new URL(originalUrl, htmlFileCdnUrl);
+          resolvedCdnUrl.search = "";
+          resolvedCdnUrl.searchParams.set("h", fileHash);
+          $(element).attr(attributeName, resolvedCdnUrl.toString());
+        }
+      })
+    )
 
-  await fs.writeFile(htmlFilePath, $.html());
+  await Promise.all([
+    processTag("script", "src"),
+    processTag("link[rel=stylesheet]", "href"),
+    processTag("img", "src")
+  ]);
+
+  await fs.promises.writeFile(htmlFilePath, $.html());
   console.log(`Processed: ${htmlFilePath}`);
 };
